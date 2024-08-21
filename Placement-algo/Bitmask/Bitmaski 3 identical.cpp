d251fa0af582755497336eb2152bfd50324f23c9
dhanesh jain <157800301+dhaneshjain026@users.noreply.github.com>
2024-08-21 21:55:40 +0530

    Update Bitmaski 3 identical.cpp

--- Placement-algo/Bitmask/Bitmaski 3 identical.cpp
+++ Placement-algo/Bitmask/Bitmaski 3 identical.cpp
@@ -12,18 +12,44 @@ then the required answer has the bit set at that position. */
 #include<bits/stdc++.h>
 typedef long long lli;
 using namespace std;
+lli mod=1e9+7;
+
+int find_single_occurence(vector<int> &arr)
+{
+    int n=arr.size();
+    int ans=0;
+    for(int i=0;i<32;i++)   // looping through each digit
+    {
+        int sum=0;
+        for(int j=0;j<n;j++) // looping through each element
+        {
+            sum+=(arr[j] & (1<<i));
+        }
+        if(sum%3)           
+        {
+            ans|=(1<<i);    // set the bit in answer
+        }
+    }
+    return ans;   
+}
+
+void solve()
+{
+    vector<int> arr={23, 5, 23, 4, 23, 4, 5, 3, 5, 4};
+    int ans = find_single_occurence(arr);
+    cout<<ans<<endl;
+}
 
 int main()
 {
-    unsigned int x = 7; 
-    for(int i=31;i>=0;i--) // looping through each digit from MSB to LSB
+    lli t;
+    cin>>t;
+    while(t--)
     {
-        if(x&(1<<i))       // checking current bit is set or not
-        {
-            cout << '1';
-        }
-        else cout << '0';
+        solve();
     }
-    cout<<endl;
     return 0; 
 }
+
+
+ 
